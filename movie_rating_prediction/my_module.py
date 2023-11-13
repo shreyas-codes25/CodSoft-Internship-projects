532a0ccdbe50ba8d775ded1b148c4c805fe5c15c
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2023-11-13 13:25:02 +0530

    Update my_module.py

--- movie_rating_prediction/my_module.py
+++ movie_rating_prediction/my_module.py
@@ -5,23 +5,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.impute import SimpleImputer
 
 
-# Load the dataset
+
 data = pd.read_csv("Movie_Rating_Prediction\IMDbMoviesIndia.csv")
 
-# Data preprocessing
 data = data.dropna(subset=["Rating"])
 data = data.drop_duplicates(subset=["Name"])
-
-# Remove the dollar sign and 'M' from the box office column and convert it to float
 data["Votes"] = data["Votes"].str.replace(',', '', regex=True).astype(float)
-
-# Extract the year from the 'Year' column and convert it to float
 data["Year"] = data["Year"].str.extract('(\d+)').astype(float)
-
-# Convert the duration column to integer
 data["Duration"] = data["Duration"].str.extract('(\d+)').astype(float)
 
-# Label encode categorical columns
+
 label_encoders = {}
 categorical_features = ["Genre", "Director", "Actor 1", "Actor 2", "Actor 3"]
 for feature in categorical_features:
@@ -29,31 +22,22 @@ for feature in categorical_features:
     data[feature] = le.fit_transform(data[feature])
     label_encoders[feature] = le
 
-# Define the features and target variable
+
 X = data[["Year", "Duration", "Genre", "Votes", "Director", "Actor 1", "Actor 2", "Actor 3"]]
 y = data["Rating"]
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create an imputer to replace missing values with the mean of each column
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 imputer = SimpleImputer(strategy='mean')
 
-# Fit and transform the imputer on your training data
+
 X_train_imputed = imputer.fit_transform(X_train)
-
-# Use the same imputer to transform your testing data
 X_test_imputed = imputer.transform(X_test)
-
-# Create a Linear Regression model
 model = LinearRegression()
-
-# Fit the model to the imputed training data
 model.fit(X_train_imputed, y_train)
 
-# Function to predict movie rating
 def predict_movie_rating(movie_data, label_encoders):
-    # Convert the user input features into the same format as the dataset
+
     user_movie_features = [
         float(movie_data['Year']),
         label_encoders['Genre'].transform([movie_data['Genre'].capitalize()])[0],
@@ -65,11 +49,11 @@ def predict_movie_rating(movie_data, label_encoders):
         float(movie_data['Duration'])
     ]
     
-    # Use the trained model to predict the movie rating
+
     movie_rating = model.predict([user_movie_features])
     return movie_rating[0]
 
-# User input for movie data with error handling
+
 user_movie_data = {}
 try:
     user_movie_data['Year'] = float(input('Enter the Year: '))
@@ -85,6 +69,6 @@ except ValueError:
     print("Invalid input. Please enter numeric values for 'Year', 'Votes', and 'Duration'.")
     exit(1)
 
-# Predict the movie rating
+
 predicted_rating = predict_movie_rating(user_movie_data, label_encoders)
 print(f'\n\n\nThe Predicted Movie May Get a Rating of : {predicted_rating:.2f}\n\n')
